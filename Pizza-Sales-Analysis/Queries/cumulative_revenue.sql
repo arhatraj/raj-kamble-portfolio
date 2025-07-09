f215cf60f23209939f99d889dac83e30e53f2bf9
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:52:37 +0530

    Update cumulative_revenue.sql

--- Pizza-Sales-Analysis/Queries/cumulative_revenue.sql
+++ Pizza-Sales-Analysis/Queries/cumulative_revenue.sql
@@ -1,1 +1,15 @@
 
+-- Analyze the cumulative revenue generated over time.
+
+SELECT 
+	order_date, SUM(revenue) OVER (ORDER BY order_date) AS cumulative_revenue 
+FROM 
+	(SELECT o.order_date, SUM(od.quantity * p.price) AS revenue 
+FROM 
+	order_details od 
+JOIN 
+	pizzas p ON od.pizza_id = p.pizza_id 
+JOIN 
+	orders o ON od.order_id = o.order_id 
+GROUP BY 
+	o.order_date) AS sales;
