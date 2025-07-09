02991ea6990e8648fd9b1ccc346b8255f748f82f
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:42:16 +0530

    Update top_5_pizza_types.sql

--- Pizza-Sales-Analysis/Queries/top_5_pizza_types.sql
+++ Pizza-Sales-Analysis/Queries/top_5_pizza_types.sql
@@ -1,1 +1,14 @@
 
+-- List the top 5 most ordered pizza types along with their quantities.
+
+SELECT 
+    t.name, SUM(o.quantity) AS total_orders
+FROM
+    pizza_types t
+        JOIN
+    pizzas p ON t.pizza_type_id = p.pizza_type_id
+        JOIN
+    order_details o ON p.pizza_id = o.pizza_id
+GROUP BY name
+ORDER BY total_orders DESC
+LIMIT 5;  
