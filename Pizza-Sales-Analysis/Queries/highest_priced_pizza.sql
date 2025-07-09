59e68807c8b2ca6b20153f40fc95fe7ec19d21b6
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:41:14 +0530

    Update highest_priced_pizza.sql

--- Pizza-Sales-Analysis/Queries/highest_priced_pizza.sql
+++ Pizza-Sales-Analysis/Queries/highest_priced_pizza.sql
@@ -1,1 +1,11 @@
 
+-- Identify the highest-priced pizza.
+
+SELECT 
+    t.name, p.price AS highest_priced_pizza
+FROM
+    pizzas p
+        JOIN
+    pizza_types t ON p.pizza_type_id = t.pizza_type_id
+ORDER BY price DESC
+LIMIT 1;
