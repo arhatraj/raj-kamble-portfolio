374c10815eb13c0853bfb680a0326a248ce2f4d1
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:45:13 +0530

    Update avg_pizzas_per_day.sql

--- Pizza-Sales-Analysis/Queries/avg_pizzas_per_day.sql
+++ Pizza-Sales-Analysis/Queries/avg_pizzas_per_day.sql
@@ -1,1 +1,12 @@
 
+-- Group the orders by date and calculate the average number of pizzas ordered per day.
+
+SELECT 
+    AVG(quantity)
+FROM
+    (SELECT 
+        o.order_date, SUM(od.quantity) AS quantity
+    FROM
+        orders o
+    JOIN order_details od ON o.order_id = od.order_id
+    GROUP BY o.order_date) AS avg_order; 
