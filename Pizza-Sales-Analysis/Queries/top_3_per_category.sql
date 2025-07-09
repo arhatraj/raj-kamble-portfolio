8d5a021987e3a53551af57f997c91a04471c35e2
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:55:35 +0530

    Update top_3_per_category.sql

--- Pizza-Sales-Analysis/Queries/top_3_per_category.sql
+++ Pizza-Sales-Analysis/Queries/top_3_per_category.sql
@@ -1,1 +1,10 @@
 
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
+
+SELECT * FROM 
+	(SELECT pt.category, pt.name, SUM(od.quantity * p.price) AS revenue,
+	RANK() OVER (PARTITION BY pt.category ORDER BY SUM(od.quantity * p.price) DESC) AS RN
+FROM 
+	pizza_types pt JOIN pizzas p ON pt.pizza_type_id = p.pizza_type_id
+                 JOIN order_details od ON od.pizza_id = p.pizza_id
+GROUP BY pt.category, pt.name) AS ranked WHERE RN < 4;
