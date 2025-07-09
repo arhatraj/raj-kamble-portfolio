59f1dbc18a20a47f12061bbad2ec0b1a50fed2a2
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:45:59 +0530

    Update top_3_pizzas_by_revenue.sql

--- Pizza-Sales-Analysis/Queries/top_3_pizzas_by_revenue.sql
+++ Pizza-Sales-Analysis/Queries/top_3_pizzas_by_revenue.sql
@@ -1,1 +1,13 @@
 
+-- Determine the top 3 most ordered pizza types based on revenue.
+
+SELECT 
+    pt.name, SUM(o.quantity * p.price) AS revenue
+FROM
+    pizzas p
+        JOIN
+    pizza_types pt ON pt.pizza_type_id = p.pizza_type_id
+        JOIN
+    order_details o ON p.pizza_id = o.pizza_id
+GROUP BY pt.name
+ORDER BY revenue DESC LIMIT 3;
