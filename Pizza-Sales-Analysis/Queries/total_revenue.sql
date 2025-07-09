956d4aa1fc845d8f4fa5fa910853bb01c48479d2
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:40:47 +0530

    Update total_revenue.sql

--- Pizza-Sales-Analysis/Queries/total_revenue.sql
+++ Pizza-Sales-Analysis/Queries/total_revenue.sql
@@ -1,1 +1,9 @@
 
+-- Calculate the total revenue generated from pizza sales.
+
+SELECT 
+    ROUND(SUM(p.price * o.quantity), 2) AS total_revenue
+FROM
+    order_details o
+        JOIN
+    pizzas p ON o.pizza_id = p.pizza_id;
