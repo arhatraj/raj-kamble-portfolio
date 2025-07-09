90d6d89dcb7b72bc5056b8e33837b6e1672fe3e6
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:43:05 +0530

    Update quantity_by_category.sql

--- Pizza-Sales-Analysis/Queries/quantity_by_category.sql
+++ Pizza-Sales-Analysis/Queries/quantity_by_category.sql
@@ -1,1 +1,14 @@
 
+-- Join the necessary tables to find the total quantity of each pizza category ordered.
+
+SELECT 
+    t.category, SUM(o.quantity) AS total_orders
+FROM
+    pizza_types t
+        JOIN
+    pizzas p ON t.pizza_type_id = p.pizza_type_id
+        JOIN
+    order_details o ON p.pizza_id = o.pizza_id
+GROUP BY category
+ORDER BY total_orders DESC
+LIMIT 5; 
