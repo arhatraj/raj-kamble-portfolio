8a677912379e1375063300845ca679aebb471602
Raj Kamble <122092807+arhatraj@users.noreply.github.com>
2025-07-09 22:41:44 +0530

    Update most_common_pizza_size.sql

--- Pizza-Sales-Analysis/Queries/most_common_pizza_size.sql
+++ Pizza-Sales-Analysis/Queries/most_common_pizza_size.sql
@@ -1,1 +1,11 @@
 
+-- Identify the most common pizza size ordered.
+
+SELECT 
+    p.size, COUNT(*) AS common_size
+FROM
+    pizzas p
+        JOIN
+    order_details o ON p.pizza_id = o.pizza_id
+GROUP BY p.size
+LIMIT 1;
